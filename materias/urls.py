3450f4a94d4b93269e02c2e1b4330fb312f01a29
pelon47 <l21120217@morelia.tecnm.mx>
2025-12-11 14:58:36 -0600

    Initial commit

--- materias/urls.py
+++ materias/urls.py
@@ -1,15 +1,65 @@
-from django.urls import path
-from . import views
-from .views import ListaMaterias, AgregarMateria, EditarMateria, EliminarMateria
+# materias/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Materia
+from .forms import MateriaForm
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('materias/', ListaMaterias.as_view(), name='lista_materias'),
-    path('materias/agregar/', AgregarMateria.as_view(), name='agregar_materia'),
-    path('materias/editar/<int:id>/', EditarMateria.as_view(), name='editar_materia'),
-    path('materias/eliminar/<int:id>/', EliminarMateria.as_view(), name='eliminar_materia'),
-    # O si prefieres la función simple:
-    # path('materias/eliminar/<int:id>/', views.eliminar_materia_simple, name='eliminar_materia'),
-    path('validar-codigo/', views.validar_codigo, name='validar_codigo'),
-    path('estadisticas/', views.estadisticas, name='estadisticas'),
-]
+# Vista simple para la página principal
+def index(request):
+    return render(request, 'materias/index.html', {
+        'total_materias': Materia.objects.count(),
+        'titulo': 'Sistema de Gestión de Materias'
+    })
+
+# Vistas basadas en clase
+class ListaMaterias(ListView):
+    model = Materia
+    template_name = 'materias/lista_materias.html'
+    context_object_name = 'materias'
+
+class AgregarMateria(CreateView):
+    model = Materia
+    form_class = MateriaForm
+    template_name = 'materias/agregar_materia.html'
+    success_url = reverse_lazy('lista_materias')
+
+class EditarMateria(UpdateView):
+    model = Materia
+    form_class = MateriaForm
+    template_name = 'materias/agregar_materia.html'  # Puede usar el mismo template
+    success_url = reverse_lazy('lista_materias')
+    
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Materia, id=id)
+
+class EliminarMateria(DeleteView):
+    model = Materia
+    success_url = reverse_lazy('lista_materias')
+    
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Materia, id=id)
+
+# Vistas de función adicionales
+def validar_codigo(request):
+    """Validar si un código de materia ya existe (para AJAX)"""
+    codigo = request.GET.get('codigo', '')
+    existe = Materia.objects.filter(codigo=codigo).exists()
+    return JsonResponse({'existe': existe})
+
+def estadisticas(request):
+    """Página de estadísticas"""
+    total = Materia.objects.count()
+    if total > 0:
+        creditos_promedio = sum(m.creditos for m in Materia.objects.all()) / total
+    else:
+        creditos_promedio = 0
+    
+    return render(request, 'materias/estadisticas.html', {
+        'total_materias': total,
+        'creditos_promedio': round(creditos_promedio, 2),
+        'materias': Materia.objects.all()[:10]
+    })
